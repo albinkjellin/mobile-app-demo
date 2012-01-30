579dd92bc0c226e8aa2779ebcb0db869ea5cc6bd
albinkjellin <albin.kjellin@mulesoft.com>
2012-01-30 11:36:43 -0800

    Initial commit

---
+++ src/main/java/SFREST.java
@@ -0,0 +1,17 @@
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+//import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
+//import javax.ws.rs.PathParam;
+
+@Path("/opportunity")
+public class SFREST {
+
+    @POST
+    @Consumes("application/xml")
+    @Path("/" )
+    public String getOpportunityList(String message) {
+        System.out.println("message = "+message);
+    	return message;
+    }
+}
